abcd05be73252c3c82a292bb96d0ec81ef8563f2
dmsirotenko <dmitriy.sirotencko@gmail.com>
2018-02-13 22:56:16 +0300

    MVC 2

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -11,8 +11,27 @@ import Foundation
 class Concentration {
     var cards = [Card]()
     
+    var indexOfOneFacedCard: Int?
+    
     func chooseCard(at index: Int) {
-        cards[index].isFacedUp = !cards[index].isFacedUp
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneFacedCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                
+                cards[index].isFacedUp = true
+                indexOfOneFacedCard = nil
+            } else {
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFacedUp = false
+                }
+                
+                cards[index].isFacedUp = true
+                indexOfOneFacedCard = index
+            }
+        }
     }
     
     init(numberOfPairs: Int) {
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -50,8 +50,17 @@ class ViewController: UIViewController {
         }
     }
     
+    var emojiChoices = ["🍬", "💀", "🎃", "👻", "🍎", "🍭", "👿", "🙀", "👹", "🦇"]
+    
+    var emoji = [Int:String]()
+    
     func emoji(for card: Card) -> String {
-        return "?"
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        
+        return emoji[card.identifier] ?? "?"
     }
 }
 
